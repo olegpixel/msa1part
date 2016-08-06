dc65a1125241e606a6e852ca7b28cd2bc561450c
Oleg <oleg.yevik@gmail.com>
2016-08-06 17:37:01 +1200

    render view

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ var app = express();
 // Use static middleware
 app.use(express.static(__dirname + '/app'));
 
+// Set the view engine
+app.set('view engine', 'html');
+
+// Set the directory that contains the views
+app.set('views', __dirname + '/app');
+
 // Create HTTP server with your app
 var http = require("http");
 var server = http.createServer(app)
