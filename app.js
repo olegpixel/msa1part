57ba3794cc37da7cae523e82c64656af3d1f7951
Oleg <oleg.yevik@gmail.com>
2016-08-06 17:48:26 +1200

    Node JS

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ var express = require("express");
 var app = express();
 
 // Use static middleware
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/public'));
 
 // Set the view engine
-app.set('view engine', 'hbs');
+app.set('view engine', 'jade');
 
 // Set the directory that contains the views
 app.set('views', __dirname + '/app');
 
 // Use the router middleware
 app.use(app.router);
+
+// Get route with one middleware
+app.get("/", function (req, res) {
+  res.send("app");
+});
 
 // Create HTTP server with your app
 var http = require("http");
