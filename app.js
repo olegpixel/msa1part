72c262fcb1e5681eb2fd730ede1ba9e0d542e9cb
Oleg <oleg.yevik@gmail.com>
2016-08-07 21:02:48 +1200

    App.js

--- app.js
+++ app.js
@@ -13,6 +13,15 @@ var app = express();
 app.set('views', path.join(__dirname, 'public'));
 app.set('view engine', 'hbs');
 
+
+app.use(function (req , res ,next) {
+  res.setHeader('Access-Control-Allow-Origin' , '*');
+  res.setHeader('Access-Control-Allow-Methods' , 'GET,POST,PUT,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers' , 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+  next();
+});
+
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
